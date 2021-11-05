638067ff35c525ac4ba20413dd87d91b39c3f2b5
Bensuperpc <bensuperpc@gmail.com>
2021-11-05 23:04:19 +0100

    Improve tests
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- test/source/crc32_test.cpp
+++ test/source/crc32_test.cpp
@@ -8,9 +8,32 @@ TEST(crc32, basic1)
 {
   const std::string str = "";
   const auto result = 0x0;
+
+  EXPECT_EQ(result, crc32::crc32_bitwise(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_halfbyte(str.data(), str.size(), 0));
+
 #ifdef CRC32_USE_LOOKUP_TABLE_BYTE
   EXPECT_EQ(result, crc32::crc32_1byte(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_1byte_tableless(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_1byte_tableless2(str.data(), str.size(), 0));
 #endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_4
+  EXPECT_EQ(result, crc32::crc32_4bytes(str.data(), str.size(), 0));
+#endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_8
+  EXPECT_EQ(result, crc32::crc32_8bytes(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_4x8bytes(str.data(), str.size(), 0));
+
+#endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_16
+  EXPECT_EQ(result, crc32::crc32_16bytes(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_16bytes_prefetch(str.data(), str.size(), 0));
+#endif
+
+  EXPECT_EQ(result, crc32::crc32_fast(str.data(), str.size(), 0));
 }
 
 TEST(crc32, basic2)
@@ -18,9 +41,31 @@ TEST(crc32, basic2)
   const std::string str = "Linux";
   const auto result = 0x53D0684B;
 
+  EXPECT_EQ(result, crc32::crc32_bitwise(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_halfbyte(str.data(), str.size(), 0));
+
 #ifdef CRC32_USE_LOOKUP_TABLE_BYTE
   EXPECT_EQ(result, crc32::crc32_1byte(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_1byte_tableless(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_1byte_tableless2(str.data(), str.size(), 0));
 #endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_4
+  EXPECT_EQ(result, crc32::crc32_4bytes(str.data(), str.size(), 0));
+#endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_8
+  EXPECT_EQ(result, crc32::crc32_8bytes(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_4x8bytes(str.data(), str.size(), 0));
+
+#endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_16
+  EXPECT_EQ(result, crc32::crc32_16bytes(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_16bytes_prefetch(str.data(), str.size(), 0));
+#endif
+
+  EXPECT_EQ(result, crc32::crc32_fast(str.data(), str.size(), 0));
 }
 
 TEST(crc32, basic3)
@@ -28,9 +73,31 @@ TEST(crc32, basic3)
   const std::string str = "CRC32";
   const auto result = 0xF6151584;
 
+  EXPECT_EQ(result, crc32::crc32_bitwise(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_halfbyte(str.data(), str.size(), 0));
+
 #ifdef CRC32_USE_LOOKUP_TABLE_BYTE
   EXPECT_EQ(result, crc32::crc32_1byte(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_1byte_tableless(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_1byte_tableless2(str.data(), str.size(), 0));
 #endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_4
+  EXPECT_EQ(result, crc32::crc32_4bytes(str.data(), str.size(), 0));
+#endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_8
+  EXPECT_EQ(result, crc32::crc32_8bytes(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_4x8bytes(str.data(), str.size(), 0));
+
+#endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_16
+  EXPECT_EQ(result, crc32::crc32_16bytes(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_16bytes_prefetch(str.data(), str.size(), 0));
+#endif
+
+  EXPECT_EQ(result, crc32::crc32_fast(str.data(), str.size(), 0));
 }
 
 TEST(crc32, basic4)
@@ -38,9 +105,31 @@ TEST(crc32, basic4)
   const std::string str = "0123456789";
   const auto result = 0xA684C7C6;
 
+  EXPECT_EQ(result, crc32::crc32_bitwise(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_halfbyte(str.data(), str.size(), 0));
+
 #ifdef CRC32_USE_LOOKUP_TABLE_BYTE
   EXPECT_EQ(result, crc32::crc32_1byte(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_1byte_tableless(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_1byte_tableless2(str.data(), str.size(), 0));
 #endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_4
+  EXPECT_EQ(result, crc32::crc32_4bytes(str.data(), str.size(), 0));
+#endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_8
+  EXPECT_EQ(result, crc32::crc32_8bytes(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_4x8bytes(str.data(), str.size(), 0));
+
+#endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_16
+  EXPECT_EQ(result, crc32::crc32_16bytes(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_16bytes_prefetch(str.data(), str.size(), 0));
+#endif
+
+  EXPECT_EQ(result, crc32::crc32_fast(str.data(), str.size(), 0));
 }
 
 TEST(crc32, basic5)
@@ -48,9 +137,31 @@ TEST(crc32, basic5)
   const std::string str = "LGBTQIA+";
   const auto result = 0x5C9BD8DC;
 
+  EXPECT_EQ(result, crc32::crc32_bitwise(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_halfbyte(str.data(), str.size(), 0));
+
 #ifdef CRC32_USE_LOOKUP_TABLE_BYTE
   EXPECT_EQ(result, crc32::crc32_1byte(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_1byte_tableless(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_1byte_tableless2(str.data(), str.size(), 0));
 #endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_4
+  EXPECT_EQ(result, crc32::crc32_4bytes(str.data(), str.size(), 0));
+#endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_8
+  EXPECT_EQ(result, crc32::crc32_8bytes(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_4x8bytes(str.data(), str.size(), 0));
+
+#endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_16
+  EXPECT_EQ(result, crc32::crc32_16bytes(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_16bytes_prefetch(str.data(), str.size(), 0));
+#endif
+
+  EXPECT_EQ(result, crc32::crc32_fast(str.data(), str.size(), 0));
 }
 
 TEST(crc32, basic6)
@@ -59,7 +170,29 @@ TEST(crc32, basic6)
   const auto result = 0x5DD186B5;
 
   // EXPECT_NE(0x0, crc32::crc32_1byte(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_bitwise(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_halfbyte(str.data(), str.size(), 0));
+
 #ifdef CRC32_USE_LOOKUP_TABLE_BYTE
   EXPECT_EQ(result, crc32::crc32_1byte(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_1byte_tableless(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_1byte_tableless2(str.data(), str.size(), 0));
 #endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_4
+  EXPECT_EQ(result, crc32::crc32_4bytes(str.data(), str.size(), 0));
+#endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_8
+  EXPECT_EQ(result, crc32::crc32_8bytes(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_4x8bytes(str.data(), str.size(), 0));
+
+#endif
+
+#ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_16
+  EXPECT_EQ(result, crc32::crc32_16bytes(str.data(), str.size(), 0));
+  EXPECT_EQ(result, crc32::crc32_16bytes_prefetch(str.data(), str.size(), 0));
+#endif
+
+  EXPECT_EQ(result, crc32::crc32_fast(str.data(), str.size(), 0));
 }
