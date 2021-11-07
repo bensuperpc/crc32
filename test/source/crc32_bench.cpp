380f2db886b5ef600cd8941aff5992757df8524b
Bensuperpc <bensuperpc@gmail.com>
2021-11-07 20:32:06 +0100

    Improve speed on perfs bench
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- test/source/crc32_bench.cpp
+++ test/source/crc32_bench.cpp
@@ -6,41 +6,41 @@
 //
 
 #include <cstdint>
-#include <random>
-#include <string>
-#include <vector>
 
 #include "crc32/crc32.hpp"
 
 #include <benchmark/benchmark.h>
 
-static std::string generate(const std::uint64_t length)
+static char* generate(const std::uint64_t length);
+
+static char* generate(const std::uint64_t length)
 {
-  std::string possible_characters =
-      "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  std::random_device rd;
-  std::mt19937 engine(rd());
-  std::uniform_int_distribution<> dist(0, possible_characters.size() - 1);
-  std::string ret = "";
-  for (std::uint64_t i = 0; i < length; i++) {
-    ret += possible_characters[dist(engine)];
+  uint32_t randomNumber = 0x27121978;
+  // initialize
+  char* data = new char[length];
+  for (size_t i = 0; i < length; i++) {
+    data[i] = char(randomNumber & 0xFF);
+    // simple LCG, see
+    // http://en.wikipedia.org/wiki/Linear_congruential_generator
+    randomNumber = 1664525 * randomNumber + 1013904223;
   }
-  return ret;
+  return data;
 }
 
 static void crc32_fast(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
-  const std::string&& str = generate(size);
+  const char* str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_fast(str.data(), str.size(), 0);
+    crc32::crc32_fast(str, size, 0);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
+  delete[] str;
   // state.SetLabel("OK");
 }
 // Register the function as a benchmark
@@ -50,15 +50,16 @@ static void crc32_bitwise(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
-  const std::string&& str = generate(size);
+  const char* str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_bitwise(str.data(), str.size(), 0);
+    crc32::crc32_bitwise(str, size, 0);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
+  delete[] str;
 }
 // Register the function as a benchmark
 BENCHMARK(crc32_bitwise)->RangeMultiplier(100)->Range(1, 1000000000);
@@ -67,15 +68,16 @@ static void crc32_halfbyte(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
-  const std::string&& str = generate(size);
+  const char* str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_halfbyte(str.data(), str.size(), 0);
+    crc32::crc32_halfbyte(str, size, 0);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
+  delete[] str;
 }
 // Register the function as a benchmark
 BENCHMARK(crc32_halfbyte)->RangeMultiplier(100)->Range(1, 1000000000);
@@ -86,15 +88,16 @@ static void crc32_1byte(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
-  const std::string&& str = generate(size);
+  const char* str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_1byte(str.data(), str.size(), 0);
+    crc32::crc32_1byte(str, size, 0);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
+  delete[] str;
 }
 // Register the function as a benchmark
 BENCHMARK(crc32_1byte)->RangeMultiplier(100)->Range(1, 1000000000);
@@ -103,15 +106,16 @@ static void crc32_1byte_tableless(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
-  const std::string&& str = generate(size);
+  const char* str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_1byte_tableless(str.data(), str.size(), 0);
+    crc32::crc32_1byte_tableless(str, size, 0);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
+  delete[] str;
 }
 // Register the function as a benchmark
 BENCHMARK(crc32_1byte_tableless)->RangeMultiplier(100)->Range(1, 1000000000);
@@ -120,15 +124,16 @@ static void crc32_1byte_tableless2(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
-  const std::string&& str = generate(size);
+  const char* str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_1byte_tableless2(str.data(), str.size(), 0);
+    crc32::crc32_1byte_tableless2(str, size, 0);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
+  delete[] str;
 }
 // Register the function as a benchmark
 BENCHMARK(crc32_1byte_tableless2)->RangeMultiplier(100)->Range(1, 1000000000);
@@ -141,15 +146,16 @@ static void crc32_4bytes(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
-  const std::string&& str = generate(size);
+  const char* str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_4bytes(str.data(), str.size(), 0);
+    crc32::crc32_4bytes(str, size, 0);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
+  delete[] str;
 }
 // Register the function as a benchmark
 BENCHMARK(crc32_4bytes)->RangeMultiplier(100)->Range(1, 1000000000);
@@ -162,15 +168,16 @@ static void crc32_8bytes(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
-  const std::string&& str = generate(size);
+  const char* str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_8bytes(str.data(), str.size(), 0);
+    crc32::crc32_8bytes(str, size, 0);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
+  delete[] str;
 }
 // Register the function as a benchmark
 BENCHMARK(crc32_8bytes)->RangeMultiplier(100)->Range(1, 1000000000);
@@ -179,15 +186,16 @@ static void crc32_4x8bytes(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
-  const std::string&& str = generate(size);
+  const char* str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_4x8bytes(str.data(), str.size(), 0);
+    crc32::crc32_4x8bytes(str, size, 0);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
+  delete[] str;
 }
 // Register the function as a benchmark
 BENCHMARK(crc32_4x8bytes)->RangeMultiplier(100)->Range(1, 1000000000);
@@ -200,15 +208,16 @@ static void crc32_16bytes(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
-  const std::string&& str = generate(size);
+  const char* str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_16bytes(str.data(), str.size(), 0);
+    crc32::crc32_16bytes(str, size, 0);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
+  delete[] str;
 }
 // Register the function as a benchmark
 BENCHMARK(crc32_16bytes)->RangeMultiplier(100)->Range(1, 1000000000);
@@ -217,15 +226,16 @@ static void crc32_16bytes_prefetch(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
-  const std::string&& str = generate(size);
+  const char* str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_16bytes(str.data(), str.size(), 0);
+    crc32::crc32_16bytes(str, size, 0);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
+  delete[] str;
 }
 // Register the function as a benchmark
 BENCHMARK(crc32_16bytes_prefetch)->RangeMultiplier(100)->Range(1, 1000000000);
