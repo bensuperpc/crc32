b93525fe9c1e88e690be62a725b815c942b33e21
Bensuperpc <bensuperpc@gmail.com>
2021-11-09 10:16:14 +0100

    Update benchmark
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- test/source/crc32_bench.cpp
+++ test/source/crc32_bench.cpp
@@ -28,208 +28,231 @@ static std::unique_ptr<char[]> generate(const std::uint64_t length)
   return data;
 }
 
-static void crc32_fast(benchmark::State& state)
+static void crc32_fast_bench(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
   auto str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_fast(str.get(), size, 0);
-    // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
+    crc32::crc32_fast(str.get(), size, 0);
+
+    benchmark::ClobberMemory();
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
 
   // state.SetLabel("OK");
 }
-// Register the function as a benchmark
-BENCHMARK(crc32_fast)->RangeMultiplier(100)->Range(1, 1000000000);
+BENCHMARK(crc32_fast_bench)
+    ->Name("crc32_fast")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000);
 
-static void crc32_bitwise(benchmark::State& state)
+static void crc32_bitwise_bench(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
   auto str = generate(size);
 
   for (auto _ : state) {
+    benchmark::DoNotOptimize(str);
     crc32::crc32_bitwise(str.get(), size, 0);
-    // Make sure the variable is not optimized away by compiler
-    benchmark::DoNotOptimize(str);
+    benchmark::ClobberMemory();
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
 }
-// Register the function as a benchmark
-BENCHMARK(crc32_bitwise)->RangeMultiplier(100)->Range(1, 1000000000);
+BENCHMARK(crc32_bitwise_bench)
+    ->Name("crc32_bitwise")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000);
 
-static void crc32_halfbyte(benchmark::State& state)
+static void crc32_halfbyte_bench(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
   auto str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_halfbyte(str.get(), size, 0);
-    // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
+    crc32::crc32_halfbyte(str.get(), size, 0);
+    benchmark::ClobberMemory();
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
 }
-// Register the function as a benchmark
-BENCHMARK(crc32_halfbyte)->RangeMultiplier(100)->Range(1, 1000000000);
+BENCHMARK(crc32_halfbyte_bench)
+    ->Name("crc32_halfbyte")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000);
 
 #ifdef CRC32_USE_LOOKUP_TABLE_BYTE
 
-static void crc32_1byte(benchmark::State& state)
+static void crc32_1byte_bench(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
   auto str = generate(size);
 
   for (auto _ : state) {
+    benchmark::DoNotOptimize(str);
     crc32::crc32_1byte(str.get(), size, 0);
-    // Make sure the variable is not optimized away by compiler
-    benchmark::DoNotOptimize(str);
+    benchmark::ClobberMemory();
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
 }
-// Register the function as a benchmark
-BENCHMARK(crc32_1byte)->RangeMultiplier(100)->Range(1, 1000000000);
+BENCHMARK(crc32_1byte_bench)
+    ->Name("crc32_1byte")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000);
 
-static void crc32_1byte_tableless(benchmark::State& state)
+static void crc32_1byte_tableless_bench(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
   auto str = generate(size);
 
   for (auto _ : state) {
+    benchmark::DoNotOptimize(str);
     crc32::crc32_1byte_tableless(str.get(), size, 0);
-    // Make sure the variable is not optimized away by compiler
-    benchmark::DoNotOptimize(str);
+    benchmark::ClobberMemory();
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
 }
-// Register the function as a benchmark
-BENCHMARK(crc32_1byte_tableless)->RangeMultiplier(100)->Range(1, 1000000000);
+BENCHMARK(crc32_1byte_tableless_bench)
+    ->Name("crc32_1byte_tableless")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000);
 
-static void crc32_1byte_tableless2(benchmark::State& state)
+static void crc32_1byte_tableless2_bench(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
   auto str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_1byte_tableless2(str.get(), size, 0);
-    // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
+    crc32::crc32_1byte_tableless2(str.get(), size, 0);
+    benchmark::ClobberMemory();
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
 }
-// Register the function as a benchmark
-BENCHMARK(crc32_1byte_tableless2)->RangeMultiplier(100)->Range(1, 1000000000);
+BENCHMARK(crc32_1byte_tableless2_bench)
+    ->Name("crc32_1byte_tableless2")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000);
 
 #endif
 
 #ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_4
 
-static void crc32_4bytes(benchmark::State& state)
+static void crc32_4bytes_bench(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
   auto str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_4bytes(str.get(), size, 0);
-    // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
+    crc32::crc32_4bytes(str.get(), size, 0);
+    benchmark::ClobberMemory();
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
 }
-// Register the function as a benchmark
-BENCHMARK(crc32_4bytes)->RangeMultiplier(100)->Range(1, 1000000000);
+BENCHMARK(crc32_4bytes_bench)
+    ->Name("crc32_4bytes")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000);
 
 #endif
 
 #ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_8
 
-static void crc32_8bytes(benchmark::State& state)
+static void crc32_8bytes_bench(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
   auto str = generate(size);
 
   for (auto _ : state) {
+    benchmark::DoNotOptimize(str);
     crc32::crc32_8bytes(str.get(), size, 0);
-    // Make sure the variable is not optimized away by compiler
-    benchmark::DoNotOptimize(str);
+    benchmark::ClobberMemory();
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
 }
-// Register the function as a benchmark
-BENCHMARK(crc32_8bytes)->RangeMultiplier(100)->Range(1, 1000000000);
+BENCHMARK(crc32_8bytes_bench)
+    ->Name("crc32_8bytes")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000);
 
-static void crc32_4x8bytes(benchmark::State& state)
+static void crc32_4x8bytes_bench(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
   auto str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_4x8bytes(str.get(), size, 0);
-    // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
+    crc32::crc32_4x8bytes(str.get(), size, 0);
+    benchmark::ClobberMemory();
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
 }
-// Register the function as a benchmark
-BENCHMARK(crc32_4x8bytes)->RangeMultiplier(100)->Range(1, 1000000000);
+BENCHMARK(crc32_4x8bytes_bench)
+    ->Name("crc32_4x8bytes")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000);
 
 #endif
 
 #ifdef CRC32_USE_LOOKUP_TABLE_SLICING_BY_16
 
-static void crc32_16bytes(benchmark::State& state)
+static void crc32_16bytes_bench(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
   auto str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_16bytes(str.get(), size, 0);
-    // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
+    crc32::crc32_16bytes(str.get(), size, 0);
+    benchmark::ClobberMemory();
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
 }
-// Register the function as a benchmark
-BENCHMARK(crc32_16bytes)->RangeMultiplier(100)->Range(1, 1000000000);
+BENCHMARK(crc32_16bytes_bench)
+    ->Name("crc32_16bytes")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000);
 
-static void crc32_16bytes_prefetch(benchmark::State& state)
+static void crc32_16bytes_prefetch_bench(benchmark::State& state)
 {
   // Code inside this loop is measured repeatedly
   auto size = state.range(0);
   auto str = generate(size);
 
   for (auto _ : state) {
-    crc32::crc32_16bytes(str.get(), size, 0);
-    // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(str);
+    crc32::crc32_16bytes_prefetch(str.get(), size, 0, 512);
+    benchmark::ClobberMemory();
   }
   // state.SetItemsProcessed(state.iterations() * state.range(0));
   state.SetBytesProcessed(state.iterations() * state.range(0) * sizeof(char));
 }
-// Register the function as a benchmark
-BENCHMARK(crc32_16bytes_prefetch)->RangeMultiplier(100)->Range(1, 1000000000);
+BENCHMARK(crc32_16bytes_prefetch_bench)
+    ->Name("crc32_16bytes")
+    ->RangeMultiplier(100)
+    ->Range(1, 1000000000);
 
 #endif
 
