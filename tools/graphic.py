71cd49933350c1d55b88b2e3571ef86e38924dce
Bensuperpc <bensuperpc@gmail.com>
2021-11-12 20:39:46 +0100

    Improve CI speed and graphic
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- tools/graphic.py
+++ tools/graphic.py
@@ -2,12 +2,23 @@
 
 from argparse import ArgumentParser
 from itertools import groupby
+from cycler import cycler
+from random import randint
 import json
 import math
 import operator
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-plt.rcParams['figure.figsize'] = [21, 12]
+
+def generate_color(size):
+    colors = []
+
+    for i in range(size):
+        colors.append('#%06X' % randint(0, 0xFFFFFF))
+
+    colors = sorted(set(colors), key=colors.index)  # Remove all same elements
+    return colors
 
 
 def extract_label_from_benchmark(benchmark):
@@ -25,6 +36,9 @@ def extract_size_from_benchmark(benchmark):
 
 
 if __name__ == "__main__":
+    plt.rcParams['figure.figsize'] = [21, 12]
+    mpl.rcParams['axes.prop_cycle'] = cycler(color=generate_color(200))
+
     parser = ArgumentParser()
     parser.add_argument('path', help='benchmark result json file')
     args = parser.parse_args()
@@ -40,7 +54,7 @@ if __name__ == "__main__":
             #log_y = list(map(math.log, y))
             plt.plot(x, y, label=key, marker=None)
 
-        plt.grid(color='green', linestyle='--', linewidth=0.2) # Add grid
+        plt.grid(color='green', linestyle='--', linewidth=0.2)  # Add grid
         plt.xlabel('Array size')
         plt.ylabel('Gigabyte per second (GB/s)')
         plt.title('CRC32 Algorithm Benchmark')
