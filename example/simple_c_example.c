ff0b1364f3f0004c0fa1c265e75511eea45b99b9
Bensuperpc <bensuperpc@gmail.com>
2021-11-13 12:05:57 +0100

    Add C only function
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

---
+++ example/simple_c_example.c
@@ -0,0 +1,28 @@
+// //////////////////////////////////////////////////////////
+// simple_example.c
+// Copyright (c) 2021-2021 Stephan Brumme. All rights reserved.
+// Simple example contributed by Bensuperpc
+// see http://create.stephan-brumme.com/disclaimer.html
+//
+
+#include <stdint.h>
+#include <stdio.h>
+
+#include "crc32/crc32.h"
+
+int main(int argc, char* argv[])
+{
+  char str[6] = {"Linux"};
+
+  const uint32_t result = crc32_halfbyte(str, 6, 0);
+
+  printf("0x%04x", result);
+  /*
+  printf("%zu\n", sz);
+
+  std::cout << "CRC32 of: " << str << " = 0x" << std::hex << result
+            << std::endl;
+            */
+
+  return 0;
+}
--- example/simple_example.cpp
+++ example/simple_example.cpp
@@ -12,10 +12,16 @@
 
 auto main(int argc, char* argv[]) -> int
 {
-  const std::string str = "Linux";
-  const auto result = crc32::crc32_halfbyte(str.data(), str.size(), 0);
+  const std::string str1 = "Linux";
+  const auto result1 =
+      crc32::crc32_halfbyte(str1.data(), str1.size(), 0);  // Use void* for data
+  std::cout << "CRC32 of: " << str1 << " = 0x" << std::hex << result1
+            << std::endl;
 
-  std::cout << "CRC32 of: " << str << " = 0x" << std::hex << result
+  const std::string str2 = "Linux";
+  const auto result2 = crc32::crc32_halfbyte(
+      str2, 0);  // Use std::string_view or std::string for data
+  std::cout << "CRC32 of: " << str2 << " = 0x" << std::hex << result2
             << std::endl;
 
   return 0;
